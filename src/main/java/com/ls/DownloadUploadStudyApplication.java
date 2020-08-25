5d36778f32c9b28e96b8fc98436bcfe18563190a
ls <18041878024@163.com>
2020-08-25 11:36:06 +0800

    创建一个空的springboot web项目 需要thymeleaf模板

---
+++ src/main/java/com/ls/DownloadUploadStudyApplication.java
@@ -0,0 +1,13 @@
+package com.ls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadUploadStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DownloadUploadStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ls/download_upload_study/DownloadUploadStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ls.download_upload_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadUploadStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
